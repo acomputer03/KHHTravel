858aec37f671f55961699f15f592c22e0fc4c4fe
YI-RU <acomputer03@gmail.com>
2021-03-21 09:32:13 +0800

    完成基本分頁功能

--- all.js
+++ all.js
@@ -1,23 +1,92 @@
 var travel = new Vue({
   el: "#travel",
   data: {
-    data:[],//預先定義變數
-
+    data: [], //預先定義變數
+    Zonedatas:[],
+    currentPage: 0,
+    locations:[],
+    currentLocation:''
   },
-  computed:{
-      filterData(){
-        const vm = this;
-        return vm.data;
+  methods: {
+    getUniquePage() {
+      //陣列內容不得重複
+      const locations = new Set();
+      const vm = this;
+      vm.data.forEach((item, i) => {
+        
+        locations.add(item.Zone);
+      });
+      console.log(locations);
+      vm.locations = Array.from(locations);
+    },
+  },
+  computed: {
+    filterData() {
+      const vm = this;
+      //先過濾
+      let items=[];
+      if(vm.currentLocation !==''){
+        items = vm.data.filter((item,i)=>{
+          return item.Zone == vm.currentLocation
+        })
+      }else{
+        items = vm.data
       }
+
+
+      //再分頁
+
+      const newData = [];
+      console.log(vm.currentLocation )
+      items.forEach((item, i) => {
+        if (i % 10 === 0) {
+          //取餘數
+          newData.push([]); //將頁數先訂出來
+        }
+
+        const page = parseInt(i / 10); //取整數
+        newData[page].push(item);
+      });
+      // console.log( newData)
+      return newData;
+    },
   },
   created() {
     const vm = this;
-    axios.get('https://api.kcg.gov.tw/api/service/get/9c8e1450-e833-499c-8320-29b36b7ace5c')
+    axios
+      .get(
+        "https://api.kcg.gov.tw/api/service/get/9c8e1450-e833-499c-8320-29b36b7ace5c"
+      )
       .then(function (response) {
         // handle success接收數據成功
         console.log(response);
-        vm.data = response.data.data.XML_Head.Infos.Info;
-        console.log(vm.data);
+        vm.data = response.data.data.XML_Head.Infos.Info;  
+        axios
+          .get("Zone.json")
+          .then(function (response) {  
+            vm.Zonedatas = response.data;
+
+            vm.data.forEach((item)=>{
+              for( Zonedata in vm.Zonedatas){
+                
+                if(item.Zipcode == vm.Zonedatas[Zonedata].Zipcode){
+                  // console.log( vm.Zonedatas[Zonedata].Zipcode);
+                  item.Zone = vm.Zonedatas[Zonedata].Zone;
+                }
+              }
+              
+              
+            })
+
+            
+            console.log(vm.data);
+            vm.getUniquePage();
+
+          })
+          .catch(function (response) {
+            // console.log(response);
+          });
+          
       })
       .catch(function (error) {
         // handle error接收數據失敗
